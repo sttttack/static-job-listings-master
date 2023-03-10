2f5f81a363d6f11049a85a2a755bd0264a200faa
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-03-10 01:18:12 -0800

    added filter function to the main.js file

--- main.js
+++ main.js
@@ -3,43 +3,86 @@
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
+    const mainHTML = document.querySelector(".main");
+    const arrayData = data;
+    const myArray = data.map((element) => {
+      var langElement = element.languages;
+      var toolsElement = element.tools;
+      var stringNew = element.new.toString();
 
-    const mainHTML = document.querySelector(".main")
-    const myData = (data);
-
-
-    const myArray = data.map(element => {
-
-      
-      let stringNew = element.new.toString();
-      if (stringNew === 'true') {
-        var newString = `<div id="newString">New!</div>`
+      if (stringNew === "true") {
+        var newString = `<div id="newString">New!</div>`;
       } else {
-        newString = ``
+        newString = ``;
       }
 
-      let stringFeatured = element.featured.toString();
+      var stringFeatured = element.featured.toString();
 
-      if (stringFeatured === 'true') {
-        var featuredString = `<div id="featuredString">Featured!</div>`
-      } else 
-        featuredString = ``
+      if (stringFeatured === "true") {
+        var featuredString = `<div id="featuredString">Featured!</div>`;
+      } else featuredString = ``;
 
-      return mainHTML.innerHTML += `<div class="box">
+      mainHTML.innerHTML += `<div class="box">
       <img id="profile" src="${element.logo}">
       <div id="first_div"><h6>${element.company}</h6>
       ${newString}
       ${featuredString}</div>
       <p>${element.position}</p>
-      <h5 id="small"> ${element.postedAt} &#x2022 ${element.contract} &#x2022 ${element.location}</h5>
+      <h5 id="small"> ${element.postedAt} &#x2022 ${element.contract} &#x2022 ${
+        element.location
+      }</h5>
       <div id="hr"></div>
       <div id="tools">
       <div id="small_boxes"><h5>${element.role}</h5></div> 
-      <div id="small_boxes"><h5>${element.level}</h5></div>   
-      ${element.languages.map(language =>`<div id="small_boxes"><h5>${language}</h5></div>`).join("")}
-      ${element.tools.map(tool =>`<div id="small_boxes"><h5>${tool}</h5></div>`).join("")}
+      <div id="small_boxes"><h5>${element.level}</h5></div>  
+      ${langElement
+        .map((language) => `<div id="small_boxes"><h5>${language}</h5></div>`)
+        .join("")}
+      ${toolsElement
+        .map((tool) => `<div id="small_boxes"><h5>${tool}</h5></div>`)
+        .join("")}
       </div>
-      </div>`
-    })
-    
-});
+      </div>`;
+
+      // This part of code is responsible for filtering Language Arrays
+
+      const upperDiv = document.querySelector(".upper_div");
+      const boxes = document.querySelectorAll(".box");
+      const smallBox = document.querySelectorAll("#small_boxes");
+      const filterDiv = document.querySelector(".upper_div");
+
+      for (var i = 0; i < smallBox.length; i++) {
+        smallBox[i].addEventListener("click", (e) => {
+          var targetValue = e.target.innerHTML;
+          console.log(targetValue);
+
+          const filterLang = arrayData.filter((item) =>
+            item.languages.includes(`${targetValue}`)
+          );
+
+          const filterList = (array, element) => {
+            upperDiv.style.display = "flex";
+            if (targetValue === `${targetValue}`) {
+              const li = document.createElement("div");
+              li.innerHTML += `<div id="small_boxes"><h5>${targetValue}</h5></div>`;
+              element.append(li);
+              boxes.forEach((box) => {
+                const dataObject = arrayData.find(
+                  (item) => item.company === box.querySelector("h6").textContent
+                );
+                if (
+                  !dataObject.languages.includes(`${targetValue}`) &&
+                  !dataObject.role.includes(`${targetValue}`) &&
+                  !dataObject.tools.includes(`${targetValue}`)
+                ) {
+                  box.style.display = "none";
+                }
+              });
+            }
+          };
+
+          filterList(filterLang, upperDiv);
+        });
+      }
+    });
+  });
